7ed726d818d4d06166483c77f2fc5f1b715ef88f
leen <leeneyadstudy@gmail.com>
2022-02-23 12:58:15 +0200

    v0.9
    added movement + camera script :)

---
+++ Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Playermovement();
+    }
+
+    void Playermovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+
+        transform.Translate(playerMovement, Space.Self);
+
+    }
+
+
+}   
+
