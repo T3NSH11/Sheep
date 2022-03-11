6a5f78ee20f2195b19a67fe01e5f95b072d37f24
mazen320 <mazenshaar@hotmail.com>
2022-03-11 14:40:14 +0400

    v2.2a
    - updated obstacle detection for frontside, left and right.
    now just need to make it actually steer away.

--- Assets/_Script/Sensor.cs
+++ Assets/_Script/Sensor.cs
@@ -6,31 +6,85 @@ public class Sensor : MonoBehaviour
 {
     const float sensorLength = 5;   //max distance
     const float frontSensor = 1;    //starting point of the front sensor
+    const float frontSideSensor = 0.5f;
+    const float frontSensorAngle = 30;
 
 
     public void Check()
     {
-        Vector3 sensorPosition;
+
         RaycastHit hit;     //checks if we hit something, and the angle since there are multiple sensors
-        bool flag;
-        float avoidDirection;   //direction we will need to avoid to
+        float avoidDirection = 0;   //direction we will need to avoid to
 
 
-        sensorPosition = transform.position + (transform.forward * frontSensor);
-        if (Physics.Raycast(sensorPosition, transform.forward, out hit, sensorLength))
+        Vector3 frontPosition = transform.position + (transform.forward * frontSensor);
+        if (DrawSensors(frontPosition, Vector3.forward, sensorLength, out hit))
         {
-            if(hit.normal.x < 0)
+            if (hit.normal.x < 0)
             {
-                avoidDirection = -1; //-1 will take it to the left <=
+                avoidDirection = -0.25f; //-1 will take it to the left <=
             }
             else
             {
-                avoidDirection = 1; //vice versa
+                avoidDirection = 0.25f; //vice versa
             }
-            Debug.DrawLine(transform.position, hit.point, Color.red);
+        }
+        avoidDirection -= FrontSideSensors(frontPosition, out hit, 1);  //basically the frontposition, 1, and -1 determining the left and right neded to go.
+        avoidDirection += FrontSideSensors(frontPosition, out hit, -1);
+
+    }
+    bool DrawSensors(Vector3 sensorPosition, Vector3 direction, float length, out RaycastHit hit)   //doesnt just draws, but detects
+    {
+        if (Physics.Raycast(sensorPosition, transform.forward, out hit, length))
+        {
+            Debug.DrawLine(sensorPosition, hit.point, Color.black);
+            return true; // cause it hit something
+        }
+        return false;
+    }
+
+    float FrontSideSensors(Vector3 frontPosition, out RaycastHit hit, float sensorDirection)
+    {
+        float avoidDirection = 0;
+
+        Vector3 sensorPosition = frontPosition + (transform.right * frontSideSensor * sensorDirection);     // * sensorDirection so transform.right turns transform left
+
+        Vector3 sensorAngle = Quaternion.AngleAxis(frontSensorAngle * sensorDirection, transform.up) * transform.forward;  //so we move 30 degrees to the right
+
+        if (Physics.Raycast(sensorPosition, transform.forward, out hit, sensorLength))
+        {
+            avoidDirection = 1;
+            Debug.DrawLine(sensorPosition, hit.point, Color.black);
+        }
+        if (Physics.Raycast(sensorPosition, sensorAngle, out hit, sensorLength))
+        {
+            avoidDirection = 0.5f;
+            Debug.DrawLine(sensorPosition, hit.point, Color.black);
+        }
+
+        return avoidDirection;      //so this code will be able to detect left and right, through sensordirection and figured its way its needed to go
+    }
+}
+
+/*         // how i did it before creating the float FrontSideSensors
+
+        //right sensor
+        sensorPosition = transform.position + (transform.forward * frontSensor);
+        sensorPosition += transform.right * frontSideSensor;
+
+        Vector3 rightAngle = Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward;  //so we move 30 degrees to the right
+
+        if (Physics.Raycast(sensorPosition, transform.forward, out hit, sensorLength))
+        {
+            avoidDirection = -1;
+            Debug.DrawLine(sensorPosition, hit.point, Color.black);
+        }
+        if (Physics.Raycast(sensorPosition, rightAngle, out hit, sensorLength))
+        {
+            avoidDirection = -0.5f;
+            Debug.DrawLine(sensorPosition, hit.point, Color.black);
         }
 
 
-
-    }
-}
+        //left sensor
+        */
