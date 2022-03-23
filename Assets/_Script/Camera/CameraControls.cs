b5bffeb56de775bb493ab63b2bdc9cf86de614c9
T3NSH11 <m.zaid@student.saedubai.com>
2022-03-23 14:57:53 +0400

    v3.4b
    - clamps for camera rotation

--- Assets/_Script/Camera/CameraControls.cs
+++ Assets/_Script/Camera/CameraControls.cs
@@ -21,9 +21,14 @@ public class CameraControls : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
-            transform.Rotate((Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime),(-Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime), 0, Space.World);
-            Vector3 eulerRotation = transform.rotation.eulerAngles;
-            transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, 0);
+            float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+
+            xRotation -= mouseY;
+
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
 
         }
         else
